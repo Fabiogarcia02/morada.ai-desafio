0609067ab75f3d488d2d37b205f6bb9b1107df90
Fábio Martins <fabiogarciamartins2025@gmail.com>
2026-01-20 11:04:12 -0300

    Ajustes finais

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,26 @@
+
 import express from "express";
 import cors from "cors";
+import path from "path";
 import routes from "./routes/routes";
 
 const app = express();
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
-// Middleware para habilitar CORS e JSON
+
 app.use(cors());
-app.use(express.json()); // ← ESSENCIAL
+app.use(express.json());
 
-// Rotas
+
 app.use("/caixa", routes);
 
-app.get("/", (req, res) => res.send("Servidor rodando!"));
 
-app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
+const frontendPath = path.join(__dirname, "../../frontend/build");
+app.use(express.static(frontendPath));
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
